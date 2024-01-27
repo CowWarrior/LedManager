367bacf1c9cbaeb34085fcae656526a2815850ef
Pierre-Paul Laplante <pplaplante@gmail.com>
2024-01-27 16:26:29 -0500

    Displau device info on index page

--- include/version.h
+++ include/version.h
@@ -1,10 +1,10 @@
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "7"
+  #define BUILD_NUMBER "32"
 #endif
 #ifndef VERSION
-  #define VERSION "v1.3.240127 Build 7 - 2024-01-27 14:26:26.926496"
+  #define VERSION "v1.3.240127 Build 32 - 2024-01-27 16:24:27.446111"
 #endif
 #ifndef VERSION_SHORT
-  #define VERSION_SHORT "v1.3.240127.7"
+  #define VERSION_SHORT "v1.3.240127.32"
 #endif
