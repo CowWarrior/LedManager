8b0378a74b99cfd4a75050a7e8182da5d66e5931
Pierre-Paul Laplante <pplaplante@gmail.com>
2025-01-19 19:35:25 -0500

    Added UI for Custom Pattern

--- include/version.h
+++ include/version.h
@@ -1,10 +1,10 @@
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "80"
+  #define BUILD_NUMBER "84"
 #endif
 #ifndef VERSION
-  #define VERSION "v1.3.250119 Build 80 - 2025-01-19 15:52:43.183007"
+  #define VERSION "v1.3.250119 Build 84 - 2025-01-19 19:32:35.714833"
 #endif
 #ifndef VERSION_SHORT
-  #define VERSION_SHORT "v1.3.250119.80"
+  #define VERSION_SHORT "v1.3.250119.84"
 #endif
